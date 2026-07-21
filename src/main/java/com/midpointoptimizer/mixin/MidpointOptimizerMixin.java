1422fc3dfea6ef56a38ab8ccc806db23dad7cd95
letov7482-ui <letov7482@gmail.com>
2026-07-21 18:12:09 +0300

    Create MidpointOptimizerMixin.java

---
+++ src/main/java/com/midpointoptimizer/mixin/MidpointOptimizerMixin.java
@@ -0,0 +1,16 @@
+package com.midpointoptimizer.mixin;
+
+import net.minecraft.client.render.WorldRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class MidpointOptimizerMixin {
+    
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(CallbackInfo ci) {
+        // Optimization hooks will go here
+    }
+}
