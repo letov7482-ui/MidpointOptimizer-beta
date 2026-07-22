d14745bcb6ca767ed26952d7d1871508dacb7ff2
letov7482-ui <letov7482@gmail.com>
2026-07-22 16:12:43 +0300

    Create MidpointOptimizer.java

---
+++ src/main/java/midpoint/midpointoptimizer/MidpointOptimizer.java
@@ -0,0 +1,10 @@
+package midpoint.midpointoptimizer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MidpointOptimizer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Мод инициализирован. Всё готово для оптимизации!
+    }
+}
