96702c5c3c57e120864d1a5365b3156b450735b0
letov7482-ui <letov7482@gmail.com>
2026-07-22 16:23:42 +0300

    Create OptionAccess.java

---
+++ src/main/java/midpoint/midpointoptimizer/optimization/OptionAccess.java
@@ -0,0 +1,7 @@
+package midpoint.midpointoptimizer.optimization;
+
+public class OptionAccess {
+    public static void setOption(String category, String option, Object value) {
+        // Пример доступа к видео-настройкам (как в оригинале)
+    }
+}
